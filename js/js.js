a7c2a54e38d3c28e91ddbbb7b16f95e962eba83a
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2018-01-02 14:14:06 +0530

    Fun Design
    This is a fun static website made using Typed.js

---
+++ js/js.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#typed', {
+  strings: ["Happy New Year <strong><u>Geeks!!!.</u></strong>", "This Year is going to be <strong>Exciting.</strong>", "This Year may u learn more <strong>Tech.</strong>" , "This Year may u contribute a lot to <strong>Open Source.</strong>" , "This Year may u build lot of <strong>Projects.</strong>","This year be a better <strong>Developer.</strong>","<strong>BRING IT ON.</strong>"],
+  // stringsElement: '#typed-strings',
+  typeSpeed: 100,
+  backSpeed: 20,
+  smartBack: true
+});
